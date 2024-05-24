ed2e391efaee700ce51f4b13b500a9cb9f67f5cf
Namfon <varisa.vb@hotmail.com>
2024-05-24 13:46:21 +0700

    feat(ex-6)

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,6 @@
 let height = undefined;
 let result;
+const checkValue = (val) => val ?? "Height is not defined";
 
+result = checkValue(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
